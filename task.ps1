d5104ad702b04274092941ac6cac77f0e6194a42
Pavlo Nikolaiev <workhardpolly@gmail.com>
2025-04-14 13:05:51 +0200

    solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,15 @@
-# Write your code here
+$files = Get-ChildItem "./data/"
+[array]$res = @()
+
+foreach ($file in $files) {
+    $content = Get-Content $file | ConvertFrom-Json
+    foreach ($obj in $content) {
+        if ($obj.Name -eq 'Standard_B2pts_v2') {
+            $res = $res + $obj
+        }
+    }
+}
+
+
+$res | Out-File -Path./result.json
+
